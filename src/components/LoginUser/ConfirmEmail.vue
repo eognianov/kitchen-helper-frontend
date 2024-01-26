1e173d73a99222812898ed26689b373b16e15013
ddimitrovv <dimitrov.mitko.md@gmail.com>
2024-01-26 16:37:39 +0200

    added confirmation url and pictures for it

--- src/components/LoginUser/ConfirmEmail.vue
+++ src/components/LoginUser/ConfirmEmail.vue
@@ -4,8 +4,8 @@
 			<div
                 class="col-lg-12 col-lg-8 py-5 mx-2 rightcolumn d-flex flex-column align-items-center align-self-center shadow-lg"
             >
-				<h3 v-if="isEmailConfirmed" class="email-confirmed">Email was successfully confirmed. <i class="fa-regular fa-face-smile-beam"></i><img src="/images/logos/kh-logo-hw.svg" alt></h3>
-				<h3 v-else class="email-not-confirmed">{{ errorMessage }} <i class="fa-regular fa-face-frown-open"></i></h3>
+				<h3 v-if="isEmailConfirmed" class="email-confirmed">Email was successfully confirmed.<img src="/images/confirm-email/robot-happy.svg" alt></h3>
+				<h3 v-else class="email-not-confirmed">{{ errorMessage }}<img src="/images/confirm-email/robot-unhappy.svg" alt></h3>
 			</div>
 		</div>
 	</div>
@@ -45,11 +45,25 @@
 
 <style scoped>
 .confirm-email {
+    width: 90%;
 	max-width: 650px;
 	padding: 4rem;
 	color: var(--main-text);
-    height: 300px;
-    margin-top: 5rem !important; 
+    height: fit-content;
+    display: flex;
+    flex-basis: 1;
+}
+
+.confirm-email h3 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+}
+
+.confirm-email img {
+    height: 10rem;
+    width: 10rem;
 }
 
 .email-confirmed {
@@ -59,5 +73,11 @@
 .email-not-confirmed {
     background-color: var(--dark-background);
     color: #fff;
+}
+
+@media screen and (max-width: 700px) {
+    .confirm-email h3 {
+	    flex-direction: column;
+    }
 }
 </style>
